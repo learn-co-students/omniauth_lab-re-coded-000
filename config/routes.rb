2aed6c8367cb5dbd17407b2c56ff1d3532d84a30
Ali Alrawi <ali.developer90@gmail.com>
2017-01-04 22:28:23 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+    get '/sessions/create' => 'sessions#create'
+
 
   
   # The priority is based upon order of creation: first created -> highest priority.
